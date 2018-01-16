fe340db355bd7b4f3e6580902249e21ed78f3b04
devangswami <devangswami@LEC90B11C8BF32B.trent.trentu.ca>
2018-01-16 17:05:37 -0500

    New branch

--- src/myclass.java
+++ src/myclass.java
@@ -1,7 +1,9 @@
 
 public class myclass {
 	public static void maon(String[] args){
+		int a = 10;
 		System.out.println("Hello");
 		System.out.println("Hello World");
+		System.out.println("a is " + a);
 	}
 }
